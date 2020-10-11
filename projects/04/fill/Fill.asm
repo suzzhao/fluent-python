6816340d18d2b0618cd91b37cc143bc5d54c6ae8
wowococo <wowococo@github.com>
2020-10-11 20:04:18 +0800

    implement

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,52 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// loop count: 24575-16384 + 1 = 8192
+// @8192
+// D = A
+// @R0
+// M = D   // save loop count in R0
+
+(LOOP)
+    @8192
+    D = A
+    @R0
+    M = D   // save loop count in R0
+    @KBD
+    D = M
+    @SETBLACK
+    D;JGT
+    @SETWHITE
+    D;JEQ
+
+(SETBLACK)
+    @R1
+    M = -1
+    @FILL
+    0;JMP
+
+(SETWHITE)
+    @R1
+    M = 0
+    @FILL
+    0;JMP
+
+(FILL)
+    @R0
+    MD = M - 1
+    @LOOP
+    D;JLT
+    @SCREEN
+    D = D + A
+    @R2
+    M = D
+    @R1
+    D = M
+    @R2
+    A = M
+    M = D
+    
+    @FILL
+    0;JMP
+
+
